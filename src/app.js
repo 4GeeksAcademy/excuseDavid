5795fc2870fb5555c0e8f5029865152350c735fe
MrPotatoJr <90863275+MrPotatoJr@users.noreply.github.com>
2024-12-10 01:28:25 +0000

    CualquierCosa

--- src/app.js
+++ src/app.js
@@ -9,3 +9,33 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function selecionadorExcusaRandom(lista) {
+  let random = Math.random() * lista.length;
+  let numeroRedondeado = Math.floor(random);
+  return lista[numeroRedondeado];
+}
+
+let excuse =
+  selecionadorExcusaRandom(who) +
+  " " +
+  selecionadorExcusaRandom(action) +
+  " " +
+  selecionadorExcusaRandom(what) +
+  " " +
+  selecionadorExcusaRandom(when);
+
+let excuseGenerator = document.querySelector("#excuseGenerator");
+excuseGenerator.textContent = excuse;
+console.log(excuseGenerator);
